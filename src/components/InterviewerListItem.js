004a4784e10456061776cb953edd225ac7e798a6
dimontejulia <dimontejulia@gmail.com>
2020-12-08 16:47:01 -0500

    refractoring w prop drilling

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -14,7 +14,7 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li className={interviewerClass} onClick={props.onClick}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
