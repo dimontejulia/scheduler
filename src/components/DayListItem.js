33c339345b6d5ae4656057ac55dbb013f9dcf3fb
Julia Di Monte <dimontejulia@gmail.com>
2020-12-07 21:09:38 -0500

    creating components and tests

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayListItemClass = classnames("li", {
+    "day-list__item": props,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full ": !props.spots,
+  });
+
+  const formatSpots = function (props) {
+    if (props > 1) {
+      return `${props} spots remaining`;
+    } else if (props === 1) {
+      return `${props} spot remaining`;
+    } else {
+      return `no spots remaining`;
+    }
+  };
+
+  return (
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
+import DayList from "components/DayList";
+import DayListItem from "components/DayListItem";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
